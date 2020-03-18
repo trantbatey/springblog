0a420e5acaa2598e677943a8408118a421ce11fb
trantbatey <trantbatey@gmail.com>
2020-03-17 23:11:29 -0500

    views-exercise - fixed problem including bootstrap and css.

--- src/main/java/com/codeup/springblog/SpringblogApplication.java
+++ src/main/java/com/codeup/springblog/SpringblogApplication.java
@@ -2,12 +2,22 @@ package com.codeup.springblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringblogApplication {
+public class SpringblogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringblogApplication.class, args);
+    }
+
+    /**
+     * Used when run as WAR
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringblogApplication.class);
     }
 
 }
