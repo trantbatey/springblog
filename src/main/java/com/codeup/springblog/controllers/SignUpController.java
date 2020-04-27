f8395965b5648afe512f5d26dbb589344affd762
trantbatey <trantbatey@gmail.com>
2020-04-26 20:00:57 -0500

    Took out some code from the next exercise; Added some Not Implemented pages for some inactive links.

---
+++ src/main/java/com/codeup/springblog/controllers/SignUpController.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignUpController {
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signup";
+    }
+}
