16c2a6a38555f11bd3e2d7eb241a962069436f39
trantbatey <trantbatey@gmail.com>
2020-05-14 11:03:36 -0500

    Completed Form Model Binding Lesson.

---
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -0,0 +1,37 @@
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.Ad;
+import com.codeup.springblog.models.AdRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao) {
+        this.adDao = adDao;
+    }
+
+    @GetMapping("/ads")
+    public String index(Model model) {
+        model.addAttribute("ads", adDao.findAll());
+        return "ads/index";
+    }
+
+    @GetMapping("/ads/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("ad", new Ad());
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String create(@ModelAttribute Ad ad) {
+        adDao.save(ad);
+        return "ads/index";
+    }
+}
