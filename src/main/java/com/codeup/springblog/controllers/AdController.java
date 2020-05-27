10b3cd9d8d7e8e7e69e269e0c3e180c7cd0b1627
trantbatey <trantbatey@gmail.com>
2020-05-27 02:03:20 -0500

    Added Ads and some partials that need to be there for demos coming later.

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -1,25 +1,42 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.models.Ad;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class AdController {
 
     @GetMapping("/ads")
-    @ResponseBody
-    public String showPostsIndexPage() {
-        return "ads index page";
+    public String showPostsIndexPage(Model model) {
+        List<Ad> ads = new ArrayList<>();
+        Ad ad = new Ad();
+        ad.setTitle("Car For Sale");
+        ad.setDescription("Well Maintained");
+        ads.add(ad);
+        ad = new Ad();
+        ad.setTitle("Help wanted");
+        ad.setDescription("Helping to build privacy fence.");
+        ads.add(ad);
+        model.addAttribute("ads", ads);
+        return "ads/index";
     }
 
     @GetMapping("/ads/{id}")
-    @ResponseBody
-    public String showAnIndividualPost(@PathVariable int id) {
-        return "view an individual ad";
+    public String showAnIndividualPost(@PathVariable int id, Model model) {
+        Ad ad = new Ad();
+        ad.setTitle("For Sale");
+        ad.setDescription("Get them while they last.");
+        model.addAttribute("ad", ad);
+        return "ads/show";
     }
 
     @GetMapping("/ads/create")
