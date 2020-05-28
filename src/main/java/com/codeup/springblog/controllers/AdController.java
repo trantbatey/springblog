b7f1fbbae4ed392c4c19f6892c8a692d6ca0f1d6
trantbatey <trantbatey@gmail.com>
2020-05-27 23:24:04 -0500

    Built out Ads with CRUD.

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -2,52 +2,87 @@ package com.codeup.springblog.controllers;
 
 
 import com.codeup.springblog.models.Ad;
+import com.codeup.springblog.models.AdRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao) {
+        this.adDao = adDao;
+    }
 
     @GetMapping("/ads")
-    public String showPostsIndexPage(Model model) {
-        List<Ad> ads = new ArrayList<>();
-        Ad ad = new Ad();
-        ad.setTitle("Car For Sale");
-        ad.setDescription("Well Maintained");
-        ads.add(ad);
-        ad = new Ad();
-        ad.setTitle("Help wanted");
-        ad.setDescription("Helping to build privacy fence.");
-        ads.add(ad);
-        model.addAttribute("ads", ads);
+    public String showAds(Model model) {
+        model.addAttribute("ads", adDao.findAll());
         return "ads/index";
     }
 
     @GetMapping("/ads/{id}")
-    public String showAnIndividualPost(@PathVariable int id, Model model) {
-        Ad ad = new Ad();
-        ad.setTitle("For Sale");
-        ad.setDescription("Get them while they last.");
+    public String showAd(@PathVariable long id, Model model) {
+        if (adDao.findById(id).isEmpty()) {
+            model.addAttribute("ads", adDao.findAll());
+            model.addAttribute("message", "Ad ID# " + id + " Not Found");
+            return "ads/index";
+        }
+        Ad ad = adDao.getOne(id);
         model.addAttribute("ad", ad);
         return "ads/show";
     }
 
     @GetMapping("/ads/create")
-    @ResponseBody
-    public String showFormForCreatingAPost() {
-        return "view the form for creating an ad";
+    public String getCreateAd() {
+        return "ads/create";
     }
 
     @PostMapping("/ads/create")
-    @ResponseBody
-    public String createAPost() {
-        return "create a new ad";
+    public String postCreateAd(@RequestParam(name = "title") String title,
+                               @RequestParam(name = "description") String description, Model model) {
+        Ad ad = new Ad();
+        if (title != null && description != null) {
+            ad.setTitle(title);
+            ad.setDescription(description);
+            adDao.save(ad);
+        }
+        return "redirect:/ads/" + ad.getId();
+    }
+
+    @GetMapping("/ads/edit/{id}")
+    public String getEditAd(@PathVariable long id, Model model) {
+        if (adDao.findById(id).isEmpty()) {
+            model.addAttribute("ads", adDao.findAll());
+            model.addAttribute("message", "Ad ID# " + id + " Not Found");
+            return "ads/index";
+        }
+        Ad ad = adDao.getOne(id);
+        model.addAttribute("ad", ad);
+        return "ads/edit";
+    }
+
+    @PostMapping("/ads/edit")
+    public String postEditAd(@RequestParam(name = "id") long id,
+                             @RequestParam(name = "title") String title,
+                             @RequestParam(name = "description") String description, Model model) {
+        Ad ad = new Ad();
+        if (title != null && description != null) {
+            ad.setId(id);
+            ad.setTitle(title);
+            ad.setDescription(description);
+            adDao.save(ad);
+        }
+        return "redirect:/ads/" + ad.getId();
+    }
+
+    @GetMapping("/ads/delete/{id}")
+    public String deleteAd(@PathVariable long id, Model model) {
+        adDao.deleteById(id);
+        return "redirect:/ads";
     }
 }
--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -1,9 +1,28 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="ads")
 public class Ad {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false, length = 2000)
     private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
